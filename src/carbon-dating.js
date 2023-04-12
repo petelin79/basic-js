0a5ff8769c41b4b5bb2b6cefbe783ad75fc83abd
Dmitry Petelin <petelin.dmitry@gmail.com>
2023-04-12 13:11:28 +0300

    complete carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(data) {
+  if (typeof data !== 'string' ||  data <= 0 || data > MODERN_ACTIVITY || isNaN(data)) {
+    
+    return false;
+  }
+
+  let res = Math.ceil(Math.log(MODERN_ACTIVITY / data) / (0.693 / HALF_LIFE_PERIOD))
+  
+  return res
 }
 
 module.exports = {
