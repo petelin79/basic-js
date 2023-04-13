983ac428a5d1f80682c7f52d58a911d111bbd16e
Dmitry Petelin <petelin.dmitry@gmail.com>
2023-04-13 18:53:07 +0300

    complete common char

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,10 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const func = (str) => {
+    let obj = new Object()
+    for (let i=0; i<str.length; i++){
+      if (!Object.keys(obj).includes(str[i])) {
+        obj[str[i]] = 1
+      }
+      else {
+        obj[str[i]]++
+      }
+    }
+    return obj
+  }
+
+  let a = func(s1)
+  let b = func(s2)
+
+  let counter = 0
+
+  Object.entries(a).forEach(el => {
+    Object.entries(b).forEach(elem=>{
+        if (el[0] === elem[0]){
+          counter += Math.min(el[1],elem[1])
+        }
+    })
+
+  })
+  return counter
+
 }
+
 
 module.exports = {
   getCommonCharacterCount
--- src/test.js
+++ src/test.js
@@ -1,11 +1,35 @@
-function deleteDigit(num) {
-  let max = -Math.abs(num)
-  let arr = String(num).split('')
-  arr.forEach((el,key) => {
-    let t = arr.slice(0,key).concat(arr.slice(key+1,arr.length)).join('')
-    t > max ? max = t : undefined
+function getCommonCharacterCount(s1, s2) {
+  const func = (str) => {
+    let obj = new Object()
+    for (let i=0; i<str.length; i++){
+      if (!Object.keys(obj).includes(str[i])) {
+        obj[str[i]] = 1
+      }
+      else {
+        obj[str[i]]++
+      }
+    }
+    return obj
+  }
+
+  let a = func(s1)
+  let b = func(s2)
+
+  let counter = 0
+
+  Object.entries(a).forEach(el => {
+    Object.entries(b).forEach(elem=>{
+        if (el[0] === elem[0]){
+          counter += Math.min(el[1],elem[1])
+        }
+    })
+
   })
-  return max
+  return counter
+
 }
 
-console.log( deleteDigit(342))
+
+
+
+console.log( getCommonCharacterCount('aabcc', 'adcaa'))
